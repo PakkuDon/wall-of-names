0b5b3e6e8673e75ef316be3ee3e555ab1b5433a7
PakkuDon <pakku.pd@gmail.com>
2025-04-03 22:52:47 +1100

    Set list and title when input fields are updated

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const titleInput = document.querySelector("#title-input")
+  const namesInput = document.querySelector("#names-input")
+  const titleOutput = document.querySelector("#list-title")
+  const namesOutput = document.querySelector("#names")
+
+  const render = () => {
+    const titleValue = titleInput.value.trim()
+    const names = namesInput.value.trim().split("\n")
+
+    titleOutput.textContent = titleValue
+    const listItems = names.map(name => {
+      const li = document.createElement("li")
+      li.textContent = name
+      return li
+    })
+
+    namesOutput.replaceChildren(...listItems)
+  }
+
+  // Update page when input is updated
+  titleInput.addEventListener("input", render)
+  namesInput.addEventListener("input", render)
+})
