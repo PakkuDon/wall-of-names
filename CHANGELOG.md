3ba0e001b0adf0689fd4873e4be2c933a25b9150
PakkuDon <pakku.pd@gmail.com>
2025-08-15 18:11:41 +1000

    Replace shark-terminology in JavaScript with friends
    These elements may soon display a different emoji

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## Updated
+
+- Replace references to 'sharks' with 'friends' as sharks may be replaced with other emojis
+
 ## [1.1.5] - 2025-07-24
 
 ## Added
--- sharks.js
+++ sharks.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const spawnButton = document.querySelector("#shark-button")
-  const sharkCounter = document.querySelector("#shark-counter")
+  const friendCounter = document.querySelector("#shark-counter")
 
-  // Initialise shark x,y and velocity values
-  const sharks = []
-  const sharksToSpawn = Math.floor(Math.random() * 5) + 10
-  for (let i = 0; i < sharksToSpawn; i++) {
-    sharks.push({
+  // Initialise friend x,y and velocity values
+  const friends = []
+  const initialSpawnCount = Math.floor(Math.random() * 5) + 10
+  for (let i = 0; i < initialSpawnCount; i++) {
+    friends.push({
       x: Math.floor(Math.random() * window.innerWidth),
       y: Math.floor(Math.random() * window.innerHeight),
       dX: (Math.floor(Math.random() * 50) + 10) * (Math.random() > 0.5 ? 1 : -1),
@@ -14,64 +14,64 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   }
 
-  // Add sharks to page
-  sharks.forEach(shark => {
+  // Add friends to page
+  friends.forEach(friend => {
     const elem = document.createElement("div")
     elem.className = "shark text-2xl"
-    elem.style.top = `${shark.y}px`
-    elem.style.left = `${shark.x}px`
+    elem.style.top = `${friend.y}px`
+    elem.style.left = `${friend.x}px`
     elem.style.animationDuration = `${Math.floor((Math.random() * 5) + 1)}s`
 
     document.body.appendChild(elem)
-    shark.elem = elem
+    friend.elem = elem
   })
 
-  // Move sharks around
+  // Move friends around
   let lastTick = 0
-  const moveSharks = () => {
+  const moveFriends = () => {
     const now = new Date().getTime()
     if (now - lastTick > 500) {
-      sharks.forEach(shark => {
-        // If out of bounds flip direction and reset shark coordinates to boundary
-        if (shark.x < 0) {
-          shark.dX = Math.abs(shark.dX)
-          shark.x = 0
+      friends.forEach(friend => {
+        // If out of bounds flip direction and reset friend coordinates to boundary
+        if (friend.x < 0) {
+          friend.dX = Math.abs(friend.dX)
+          friend.x = 0
         }
-        if (shark.x >= window.innerWidth) {
-          shark.dX = -Math.abs(shark.dX)
-          shark.x = window.innerWidth
+        if (friend.x >= window.innerWidth) {
+          friend.dX = -Math.abs(friend.dX)
+          friend.x = window.innerWidth
         }
-        if (shark.y < 0) {
-          shark.dY = Math.abs(shark.dY)
-          shark.y = 0
+        if (friend.y < 0) {
+          friend.dY = Math.abs(friend.dY)
+          friend.y = 0
         }
-        if (shark.y >= window.innerHeight) {
-          shark.dY = -Math.abs(shark.dY)
-          shark.y = window.innerHeight
+        if (friend.y >= window.innerHeight) {
+          friend.dY = -Math.abs(friend.dY)
+          friend.y = window.innerHeight
         }
   
-        shark.x += shark.dX
-        shark.y += shark.dY
-        shark.elem.style.top = `${shark.y}px`
-        shark.elem.style.left = `${shark.x}px`
+        friend.x += friend.dX
+        friend.y += friend.dY
+        friend.elem.style.top = `${friend.y}px`
+        friend.elem.style.left = `${friend.x}px`
   
       })
 
       lastTick = now
     }
 
-    requestAnimationFrame(moveSharks)
+    requestAnimationFrame(moveFriends)
   }
 
-  requestAnimationFrame(moveSharks)
+  requestAnimationFrame(moveFriends)
 
   let spawnButtonHeld = false
   let lastSpawn = Date.now()
   let clickX = 0
   let clickY = 0
   const availableSizes = ["md", "lg", "xl", "2xl", "3xl"]
-  const spawnShark = () => {
-    // Spawn sharks as long as button is held
+  const spawnFriend = () => {
+    // Spawn friends as long as button is held
     // Apply few ms buffer between spawns
     if (spawnButtonHeld && (Date.now() - lastSpawn) > 20) {
       lastSpawn = Date.now()
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const x = clickX || Math.floor(Math.random() * window.innerWidth)
       const y = clickY || Math.floor(Math.random() * window.innerHeight)
       const size = availableSizes[Math.floor(Math.random() * availableSizes.length)]
-      const shark = {
+      const friend = {
         x,
         y,
         dX: (Math.floor(Math.random() * 50) + 10) * (Math.random() > 0.5 ? 1 : -1),
@@ -89,22 +89,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const elem = document.createElement("div")
       elem.className = `shark text-${size}`
-      elem.style.top = `${shark.y}px`
-      elem.style.left = `${shark.x}px`
+      elem.style.top = `${friend.y}px`
+      elem.style.left = `${friend.x}px`
       elem.style.animationDuration = `${Math.floor((Math.random() * 5) + 1)}s`
 
-      shark.elem = elem
-      sharks.push(shark)
+      friend.elem = elem
+      friends.push(friend)
       document.body.appendChild(elem)
     }
-    sharkCounter.textContent = `${sharks.length} sharks`
+    friendCounter.textContent = `${friends.length} friends`
 
-    requestAnimationFrame(spawnShark)
+    requestAnimationFrame(spawnFriend)
   }
   // Start spawn loop
-  requestAnimationFrame(spawnShark)
+  requestAnimationFrame(spawnFriend)
 
-  // Keep spawning sharks as long as button is held
+  // Keep spawning friends as long as button is held
   spawnButton.addEventListener("mousedown", (event) => {
     clickX = event.clientX
     clickY = event.clientY
@@ -134,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   })
 
-  // Stop spawning sharks when button released
+  // Stop spawning friends when button released
   spawnButton.addEventListener("mouseup", () => { spawnButtonHeld = false })
   spawnButton.addEventListener("keyup", () => { spawnButtonHeld = false })
   spawnButton.addEventListener("touchend", () => { spawnButtonHeld = false })
